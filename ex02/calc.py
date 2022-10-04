20e0226c8a8dc738fbbd9b33ffa1aa28e5d4746a
sakurai <c0b21073af@edu.teu.ac.jp>
2022-10-04 15:07:53 +0900

    練習済

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,17 +1,25 @@
+from sqlite3 import OperationalError
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def click_number(event):
+def click_number(event): # 練習3
     btn =event.widget
-    num = int(btn["text"])
-    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    num = btn["text"]
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END,num) # 練習5
 
 root = tk.Tk() # 練習1
 root.geometry("300x500")
 
-r, c = 0, 0
+entry = tk.Entry(root,width=10,font=(", 40"),justify="right") # 練習4
+entry.grid(row=0,column=0,columnspan=3)
 
-for i, num in enumerate(range(9,-1,-1), 1):
+r, c = 1, 0
+
+numbers = list(range(9,-1,-1))
+operators = ["+"]
+
+for i, num in enumerate(numbers+operators, 1):
     btn = tk.Button(root,text=f"{num}", font=("",30),width=4,height=2)
     btn.bind("<1>", click_number)
     btn.grid(row=r,column=c)
@@ -19,6 +27,5 @@ for i, num in enumerate(range(9,-1,-1), 1):
     if i%3 == 0:
         r += 1
         c = 0
-
 
 root.mainloop()
