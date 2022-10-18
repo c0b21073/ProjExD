4ba8ae970e75f43abb2d7abbbe67ade4b4b600a3
sakurai <c0b21073af@edu.teu.ac.jp>
2022-10-18 14:27:22 +0900

    練習5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,11 @@
 from email.mime import image
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym #練習5
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") # 練習1
@@ -13,5 +18,8 @@ if __name__ == "__main__":
     can.create_image(cx, cy, image=bird, tag="bird") #練習3
 
     key = "" #練習4
+
+    root.bind("<KeyPress>", key_down) #練習5
+    
 
     root.mainloop()
