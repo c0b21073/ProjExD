33263881c82a610dfc84c9edd516b4ec8292528d
sakurai <c0b21073af@edu.teu.ac.jp>
2022-10-18 15:30:22 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,18 +10,31 @@ def key_up(event):
     key = "" #練習6
 
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+
+    if maze_sig[my][mx] == 0:
+        cx, cy = mx * 100 + 50, my * 100 + 50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     
     can.coords("bird", cx, cy) 
-    root.after(10, main_proc) #練習7
+    root.after(100, main_proc) #練習7
 
 
 if __name__ == "__main__":
@@ -31,8 +44,13 @@ if __name__ == "__main__":
     can = tk.Canvas(root, width=1500, height=900, bg="black")
     can.pack() #練習2
 
+    maze_sig = mm.make_maze(15, 9) #練習9
+
+    mm.show_maze(can, maze_sig) #練習10
+
     bird = tk.PhotoImage(file="fig/7.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx * 100 + 50, my * 100 + 50 #練習11
     can.create_image(cx, cy, image=bird, tag="bird") #練習3
 
     key = "" #練習4
@@ -41,10 +59,6 @@ if __name__ == "__main__":
 
     root.bind("<KeyRelease>", key_up) #練習6
 
-    root.after(100, main_proc) #練習7
-
-    maze_sig = mm.make_maze(15, 9) #練習9
-
-    mm.show_maze(can, maze_sig) #練習10
+    root.after(10,main_proc) #練習7
 
     root.mainloop()
